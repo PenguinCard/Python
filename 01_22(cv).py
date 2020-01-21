d81a6e1712b8c6dff96259ee75e08be345cc7663
PenguinCard <50713792+PenguinCard@users.noreply.github.com>
2020-01-22 03:21:51 +0900

    Update 01_22(cv).py

--- 01_22(cv).py
+++ 01_22(cv).py
@@ -7,15 +7,18 @@ Created on Wed Jan 22 01:00:30 2020
 
 import cv2
 
+# first webcam
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     
+    # camera status
     if ret == False:
         break
 
     cv2.imshow("VideoFrame", frame)
+    # press any key : end
     if cv2.waitKey(1) > 0:
         break
    
